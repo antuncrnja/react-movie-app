5eee7c219866dc4aeca5c45e3507557b3197c529
antuncrnja <antuncrnja@gmail.com>
2022-06-06 16:45:22 +0200

    update

--- src/components/pages/Popular.js
+++ src/components/pages/Popular.js
@@ -1,7 +1,6 @@
-import React, {useState, useEffect, useRef} from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import { MovieCard } from '../MovieCard'
-import { ScrollToTop } from '../ScrollToTop'
 import { useFetch } from '../useFetch'
 import { HomeLogo } from '../HomeLogo'
 
@@ -12,13 +11,12 @@ export const Popular = () => {
   const queryValue = new URLSearchParams(query).get('page')
   const [page, setPage] = useState(1)
 
-  const scrollRef = useRef(null)
-  const executeScroll = () => scrollRef.current.scrollIntoView({ behavior: 'smooth'})   
+  useEffect( ()=> {window.scrollTo({top: 0,left: 0,behavior: 'smooth'})},[page])
 
-useEffect(() =>{
-  if(queryValue === null) setPage(1) 
-    else setPage(+queryValue)
-},[queryValue])
+  useEffect(() =>{
+    if(queryValue === null) setPage(1) 
+      else setPage(+queryValue)
+  },[queryValue])
 
     const {loading, error, data} = useFetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=${page}`, page)
 
@@ -32,6 +30,7 @@ useEffect(() =>{
       <div className="overlay"></div>
      
 	<div className="container">
+    
 
     <HomeLogo />
    {/*  <Link to="/Search">
@@ -64,7 +63,7 @@ useEffect(() =>{
         </Swiper>
       </div>
 */}
-  <div className="container" style={{marginTop: 0}} ref={scrollRef}>
+  <div className="container" style={{marginTop: 0}}>
    
 
 <div className="movie-grid">
@@ -85,11 +84,11 @@ useEffect(() =>{
     <div className="paginate container" style={{width: '100%'}}>
 
       <Link to={`?page=${page-1}`}>
-       <button onClick={executeScroll} disabled={page <= 1}><span className='button-arrow'>&#x2039;</span> Previous</button>
+       <button disabled={page <= 1}><span className='button-arrow'>&#x2039;</span> Previous</button>
       </Link>
 
       <Link to={`?page=${page+1}`}>
-        <button onClick={executeScroll}>Next <span className='button-arrow'>&#x203A;	</span></button>
+        <button>Next <span className='button-arrow'>&#x203A;	</span></button>
       </Link>
 
     </div>
