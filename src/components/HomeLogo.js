1766f8979ffe7e3bbfeaf5e943ee4323809daf34
Antun <70490793+antuncrnja@users.noreply.github.com>
2024-02-28 11:14:27 +0100

    Update HomeLogo.js

--- src/components/HomeLogo.js
+++ src/components/HomeLogo.js
@@ -4,7 +4,7 @@ export const HomeLogo = () => {
   return (
     <div className="logo-wrapper">
         <h1 className="logo">The<span className='main-color'>Base</span></h1>
-        <p>It's The Movie Database</p>
+        <p>It's a Movie Database</p>
     </div>
   )
 }
